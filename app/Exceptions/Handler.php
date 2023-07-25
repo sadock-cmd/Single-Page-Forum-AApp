616d26d46c08735193ea76f463ba8df2bd2fbe8a
sadock-cmd <sadockpiason@gmail.com>
2023-07-25 12:19:20 +0300

    JWT Middleware and Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,4 +47,20 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    public function render($request, Exception $exception)
+    {
+        if($exception instanceof TokenBlacklistedException){
+            return response(['error'=>'Token can not be used, get new one']);
+        } 
+        else if($exception instanceof TokenInvalidException){
+            return response(['error'=>'Token is invalid']);
+        }
+        else if($exception instanceof TokenExpiredException){
+            return response(['error'=>'Token is expired']);
+        }
+        else if($exception instanceof JWTException){
+            return response(['error'=>'Token is not provided'], 500);
+        }
+        return parent::render($request, $exception);
+    }
 }
